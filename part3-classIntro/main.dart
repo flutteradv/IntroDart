14e23a8d5ca4032b2e2088baf99eac808759ca48
monajafi <monajafi@gmail.com>
2019-08-27 11:13:01 +0430

    getter setter added

--- part3-classIntro/main.dart
+++ part3-classIntro/main.dart
@@ -6,17 +6,33 @@ void main(){
   var c2 = Complex(3,4);
           //  ..real = 3
           //  ..imaginary = 4;
+  var r = Complex.real(5);
+  var i = Complex.imaginary(-10);
+  i.imaginary = 20;
+  i.real = 50;
+  
   print(c == c2);
   print("real num: ${c.real}");
   print("imaginary num: ${c.imaginary}");
   print(c);
+  print(r);
+  print(i);
 }
 
 class Complex {
-  num real;
-  num imaginary;
+  num _real;
+  num _imaginary;
   
-  Complex(this.real,this.imaginary);
+  get real => _real;
+  set real(num value) => _real = value;
+
+  get imaginary => _imaginary;
+  set imaginary(num value) => _imaginary = value;
+
+  Complex(this._real,this._imaginary);
+
+  Complex.real(num real): this(real,0);
+  Complex.imaginary(num imaginary): this(0,imaginary);
 
   @override
   bool operator ==(other){
